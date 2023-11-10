33e095e3524839e2661e167f79b70565c0723ada
919surya <sbb.bkc@gmail.com>
2023-11-10 18:49:11 -0500

    new branch 1

--- src/tc2/TC2.java
+++ src/tc2/TC2.java
@@ -3,12 +3,10 @@ package tc2;
 public class TC2 {
 
 	public static void main(String[] args) {
-		System.out.println("Tc2");
+		
+		
 		System.out.println("Hello world");
-		System.out.println("Tc22");
-		System.out.println("Tc23");
-		System.out.println("Tc24");
-		System.out.println("Tc25");
+
 
 		
 		
