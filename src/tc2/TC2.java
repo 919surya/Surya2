6dd9efa5d3317571677aa3d564639b52d5bfcc6c
919surya <106436569+919surya@users.noreply.github.com>
2023-11-10 18:54:54 -0500

    Update TC2.java

--- src/tc2/TC2.java
+++ src/tc2/TC2.java
@@ -7,9 +7,7 @@ public class TC2 {
 		System.out.println("Hello world");
 		System.out.println("Tc22");
 		System.out.println("Tc23");
-		System.out.println("Tc24");
-		System.out.println("Tc25");
-		System.out.println("Tc26");
+	
 
 	}
 
