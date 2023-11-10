bd304791831811ce38a1ffe3c2463e948ecfbbdd
919surya <sbb.bkc@gmail.com>
2023-11-10 18:58:45 -0500

    push ndew

--- src/tc2/TC2.java
+++ src/tc2/TC2.java
@@ -6,21 +6,19 @@ public class TC2 {
 		
 		
 		System.out.println("Hello world");
-<<<<<<< Updated upstream
+
 		System.out.println("Hello Nepal");
 		
 
 
-=======
+
 		System.out.println("Tc22");
 		System.out.println("Tc23");
 		
 		System.out.println("Hello India");
 	
 
-		
->>>>>>> Stashed changes
-		
+	
 		
 	}
 
