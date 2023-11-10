42200048912ccc829e3715afe45e7fdcb3ea197f
919surya <sbb.bkc@gmail.com>
2023-11-10 18:57:26 -0500

    push

--- src/tc2/TC2.java
+++ src/tc2/TC2.java
@@ -6,10 +6,20 @@ public class TC2 {
 		
 		
 		System.out.println("Hello world");
+<<<<<<< Updated upstream
 		System.out.println("Hello Nepal");
 		
 
 
+=======
+		System.out.println("Tc22");
+		System.out.println("Tc23");
+		
+		System.out.println("Hello India");
+	
+
+		
+>>>>>>> Stashed changes
 		
 		
 	}
